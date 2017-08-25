634dbda2d252ab0ad702d724959d38f4a218f489
Brice Shatzer <brice.shatzer@gmail.com>
2017-08-25 14:55:31 -0500

    added PlanRecommendation component and some basic transition animation styling

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Form from './components/Form';
+import PlanRecommendation from './components/PlanRecommendation';
 
 class App extends Component {
     constructor(props) {
@@ -13,15 +14,23 @@ class App extends Component {
         this.setState({formIsHidden: !this.state.formIsHidden});
     };
 
-    displayRecommendedPlan() {}
+    displayRecommendedPlan = () => {};
     
     render() {
         
         return(
-            <Form 
-                formIsHidden = {this.state.formIsHidden}
-                toggleFormVisibility = {this.toggleFormVisibility}
-            />
+            <div>
+                <Form
+                    formIsHidden = {this.state.formIsHidden}
+                    toggleFormVisibility = {this.toggleFormVisibility}
+                />
+                <PlanRecommendation
+                    toggleFormVisibility = {this.toggleFormVisibility}
+                />
+            </div>
+
+
+            
                 
         ); 
     }
---
+++ app/src/components/PlanRecommendation.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class PlanRecommendation extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    returnToForm(event) {
+        event.preventDefault();
+        this.props.toggleFormVisibility();
+    }
+    
+    render() {
+        return (
+            <section className="plan-recommendation">
+                <a href="#" id="showForm" onClick={(event) => this.returnToForm(event)}>Show Form</a>
+            </section>
+        )
+        
+    }
+}
+
+export default PlanRecommendation;
