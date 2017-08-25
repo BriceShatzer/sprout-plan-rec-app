3887cd755a62dfc6288616c00fd94df0b8294d32
Brice Shatzer <brice.shatzer@gmail.com>
2017-08-25 12:03:49 -0500

    attempting to do basic things in react that is simple in other frameworks

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import Form from './components/Form';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: "tacos",
+            formIsHidden: false
+        };
+        //this.toggleFormVisibility = this.toggleFormVisibility.bind(this);
+    }
+
+    toggleFormVisibility(){
+        //this.setState({formIsHidden: !this.state.formIsHidden});
+        alert('fired');
+    }
+    
+    
+    displayRecommendedPlan() {
+        ReactDOM.render(<App />, document.getElementById('root'));
+    }
+    
     render() {
-        return <Form />
+        
+        return(
+            <Form 
+                text={this.state.text} 
+                formIsHidden={this.state.formIsHidden}
+                toggleFormVisibility={this.toggleFormVisibility()}
+            />
+                
+        ); 
     }
 }
 
--- app/src/components/Form.js
+++ app/src/components/Form.js
@@ -2,18 +2,46 @@ import React from 'react';
 import { getRecommendedPlan } from '../lib/getRecommendedPlan'
 
 class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.formSubmit = this.formSubmit.bind(this);
 
+    }
     formSubmit(event) {
-        let formElement = document.getElementById('form');
         event.preventDefault();
-        window.alert(getRecommendedPlan(formElement));
+
+        //hide the form
+        // document.querySelector('.form').classList.add('is-hidden');
+        //this.props.setState({formIsHidden: true});
+        this.props.toggleFormVisibility();
+        //alert(this.props.text)
+
+
+
+        // get and display plan based on form inputs
+        let formElement = document.getElementById('form');
+
+        //window.alert(getRecommendedPlan(formElement));
+
+
+
+
+
+
 
         /* todo: trigger the rendering of the recommended plan page */
     }
 
     render() {
         return (
-            <section>
+            // <h3>{this.props.formIsHidden ? "form is-hidden" : "form"}</h3>
+            // <h3>{this.props.text}</h3>
+            // <section className={this.props.formIsHidden ? "form is-hidden" : "form"}>
+
+            <section className="form">
+
+                <h3>{this.props.formIsHidden ? "form is-hidden" : "form"}</h3>
+
                 <form id="form">
                     <h1>Find the Sprout Social plan that's right for you.</h1>
                     <label>How many social profiles do you manage?</label>
