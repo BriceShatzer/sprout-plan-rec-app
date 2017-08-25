15d109596a177fa384b366a6c08f4bd46d835585
Brice Shatzer <brice.shatzer@gmail.com>
2017-08-25 14:25:13 -0500

    work on tests for App.js

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,8 +1,51 @@
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  renderComponent();
 });
+
+describe('ensure that required initial state is being set correctly', () => {
+  const render = renderComponent();
+
+  test('formIsHidden is correct', () => {
+    expect(typeof render.state.formIsHidden).toBe('boolean');
+    expect(render.state.formIsHidden).toBe(false);
+  });
+
+});
+
+describe('test toggleFormVisibility method', () => {
+  const render = renderComponent();
+
+  test('successfully toggles booleans', () => {
+    testMethodEffect(false,true);
+    testMethodEffect(true,false);
+  });
+
+  test('formIsHidden = false on all other types', () => {
+    let obj = {food: ["pizza","tacos","salad"]};
+    let arr = ["inky","blinky","pinky","clyde"];
+    let str = "false";
+    let num = 42;
+
+    testMethodEffect(obj,false);
+    testMethodEffect(arr,false);
+    testMethodEffect(str,false);
+    testMethodEffect(num,false);
+  });
+
+  function testMethodEffect(intialValue, expectedValue){
+    render.setState({formIsHidden: intialValue});
+    expect(render.state.formIsHidden).toEqual(intialValue);
+
+    render.toggleFormVisibility();
+    expect(render.state.formIsHidden).toBe(expectedValue);
+  }
+});
+
+
+function renderComponent(){
+  return ReactDOM.render(<App />, document.createElement('div'));
+}
