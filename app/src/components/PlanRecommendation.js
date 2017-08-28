0f703e41c8165cfad249728cdbf7a46291243f50
Brice Shatzer <brice.shatzer@gmail.com>
2017-08-28 16:40:50 -0500

    PlanRecommendation component start

--- app/src/components/PlanRecommendation.js
+++ app/src/components/PlanRecommendation.js
@@ -1,18 +1,40 @@
 import React from 'react';
+import { getRecommendedPlanContent } from '../lib/getRecommendedPlanContent';
+
 
 class PlanRecommendation extends React.Component {
     constructor(props) {
         super(props);
     }
-    
+
     returnToForm(event) {
         event.preventDefault();
         this.props.toggleFormVisibility();
     }
     
     render() {
+        let content = getRecommendedPlanContent(this.props.planRecommendation);
+        let details = content.details;
+
+        let detailElements = Object.keys(details).map((detail,i) =>
+            <div key={i} className="detail">
+                <div>{detail}</div>
+                <div>{details[detail]}</div>
+            </div>
+        );
+
+
         return (
             <section className="plan-recommendation">
+                <h3>The plan that's right for you is...</h3>
+                <h1>{content.name}</h1>
+                <div className="value-propositions">
+                    {content.headline}
+                </div>
+                <div className="details">
+                    {detailElements}
+                </div>
+                
                 <a href="/" id="showForm" onClick={(event) => this.returnToForm(event)}>Show Form</a>
             </section>
         )
--- app/src/components/PlanRecommendation.test.js
+++ app/src/components/PlanRecommendation.test.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PlanRecommendation from './PlanRecommendation';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<PlanRecommendation />, div);
+it ('does math', () => {
+    expect(1+2).toBe(3);
 });
+
+
+// it('renders without crashing', () => {
+//     const div = document.createElement('div');
+//     ReactDOM.render(<PlanRecommendation />, div);
+// });
